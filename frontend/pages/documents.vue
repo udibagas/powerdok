ca829b72a646c22468352bf68a9151cc7132ab87
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-02-05 11:19:21 +0700

    ok

--- frontend/pages/documents.vue
+++ frontend/pages/documents.vue
@@ -5,26 +5,8 @@
 			<div class="col-12">
 				<div class="card">
 					<div class="card-body p-0">
-						<!-- <div class="row">
-              <div class="col-sm-12 col-md-6">
-                  <div id="tickets-table_length" class="dataTables_length">
-                      <label class="d-inline-flex align-items-center">
-                          Show&nbsp;
-                          <b-form-select v-model="pagination.per_page" size="sm" :options="pageSizes"></b-form-select>&nbsp;entries
-                      </label>
-                  </div>
-              </div>
-              <div class="col-sm-12 col-md-6">
-                  <div id="tickets-table_filter" class="dataTables_filter text-md-right">
-                      <label class="d-inline-flex align-items-center">
-                          Search:
-                          <b-form-input v-model="keyword" type="search" placeholder="Search..." class="form-control form-control-sm ml-2"></b-form-input>
-                      </label>
-                  </div>
-              </div>
-            </div> -->
 						<div class="table-responsive">
-							<table class="table">
+							<table class="table table-hover table-striped">
 								<thead>
 									<tr>
 										<th scope="col">Number</th>
@@ -44,24 +26,26 @@
 										<td>{{ documents.number }}</td>
 
 										<td>
-											<h5>{{ documents.title }}</h5>
+											<h5>
+												<nuxt-link to="/slug">
+													{{ documents.title }}
+												</nuxt-link>
+											</h5>
 										</td>
 										<td>{{ documents.version }}</td>
 										<td>SOP</td>
 										<td>
 											<span
 												v-for="(category, i) in documents.category"
-												class="badge bg-info text-white mr-1 p-2"
+												class="badge bg-info text-white m-1 p-2"
 												:key="i"
 												>{{ category }}</span
 											>
 										</td>
-										<!-- <td>{{ documents.departments }}</td> -->
-										<td>Department A, Department B</td>
+										<td>{{ documents.departments }}</td>
 										<td>{{ documents.effective_date }}</td>
 										<td>{{ documents.expired_date }}</td>
-										<td>bagas</td>
-										<!-- <td>{{ documents.owner_id }}</td> -->
+										<td>{{ documents.owner_id }}</td>
 										<td>
 											<button class="btn btn-sm" v-on:click="togglefav">
 												<i
@@ -115,9 +99,7 @@ export default {
     return {
       url: '/api/documents',
       title: "Documents",
-      items: [{
-          text: "Menu",
-        },
+      items: [
         {
           text: "Documents",
           active: true,
