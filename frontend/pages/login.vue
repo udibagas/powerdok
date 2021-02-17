16fec2b2433ab52f2c24763b6d3166be9d3e432b
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-02-17 10:22:15 +0700

    login ok

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -8,153 +8,217 @@ import {
  * Login component
  */
 export default {
-    layout: "auth",
-    head() {
-      return {
-        title: `${this.title}`,
-      };
-    },
-    data() {
-      return {
-        email: '',
-        password: '',
-        submitted: false,
-        title: "Powerdok | Login",
-      }
-    },
-    mounted() {
-      this.$axios.$get('/sanctum/csrf-cookie')
-    },
-    validations: {
-        email: {
-            required,
-            email,
-        },
-        password: {
-            required,
-        },
-    },
-    methods: {
-        tryToLogIn() {
-          this.submitted = true;
-          // stop here if form is invalid
-          this.$v.$touch();
+  layout: "auth",
+  head() {
+    return {
+      title: `${this.title}`,
+    };
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      submitted: false,
+      title: "Powerdok | Login",
+    }
+  },
 
-          if (this.$v.$invalid) {
-              return;
-          } else {
-              this.$auth.loginWith('laravelSanctum', {
-                data: {
-                  email: this.email,
-                  password: this.password,
-                }
-              })
-          }
-        },
-    },
+  validations: {
+    email: { required, email },
+    password: { required, },
+  },
+  methods: {
+      tryToLogIn() {
+        this.submitted = true;
+        this.$v.$touch();
+
+        if (this.$v.$invalid) {
+            return;
+        } else {
+            this.$auth.loginWith('laravelSanctum', {
+              data: {
+                email: this.email,
+                password: this.password,
+              }
+            }).then(r => {
+              this.$router.push('/');
+            }).catch(e => {
+              this.$message({
+                message: e.response.data.message,
+                type: 'error'
+              });
+            });
+        }
+      },
+  },
 };
 </script>
 
 <template>
-<div>
-    <div class="account-pages my-2 pt-sm-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <nuxt-link to="/" class="mb-3 d-block auth-logo">
-                            <img src="~/assets/images/powerdok-dark.png" alt height="40" class="logo logo-dark" />
-                            <img src="~/assets/images/powerdok-light.png" alt height="40" class="logo logo-light" />
-                        </nuxt-link>
-                    </div>
-                </div>
-            </div>
-            <div class="row align-items-center justify-content-center">
-                <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card">
-                        <div class="card-body p-4">
-                            <div class="text-center mt-2">
-                                <h5 class="text-primary">Welcome Back !</h5>
-                                <p class="text-muted">Sign in to continue to Powerdok</p>
-                            </div>
-                            <div class="p-2 mt-4">
-                                <!-- <b-alert v-model="isAuthError" variant="danger" class="mt-3" dismissible>{{ authError }}</b-alert>
+	<div>
+		<div class="account-pages my-2 pt-sm-3">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="text-center">
+							<nuxt-link to="/" class="mb-3 d-block auth-logo">
+								<img
+									src="~/assets/images/powerdok-dark.png"
+									alt
+									height="40"
+									class="logo logo-dark"
+								/>
+								<img
+									src="~/assets/images/powerdok-light.png"
+									alt
+									height="40"
+									class="logo logo-light"
+								/>
+							</nuxt-link>
+						</div>
+					</div>
+				</div>
+				<div class="row align-items-center justify-content-center">
+					<div class="col-md-8 col-lg-6 col-xl-5">
+						<div class="card">
+							<div class="card-body p-4">
+								<div class="text-center mt-2">
+									<h5 class="text-primary">Welcome Back !</h5>
+									<p class="text-muted">Sign in to continue to Powerdok</p>
+								</div>
+								<div class="p-2 mt-4">
+									<!-- <b-alert v-model="isAuthError" variant="danger" class="mt-3" dismissible>{{ authError }}</b-alert>
 
                                 <div v-if="notification.message" :class="'alert ' + notification.type">{{ notification.message }}</div> -->
 
-                                <b-form @submit.prevent="tryToLogIn">
-                                    <b-form-group id="input-group-1" label="Email" label-for="input-1">
-                                        <b-form-input id="input-1" v-model="email" type="text" placeholder="Enter email" :class="{ 'is-invalid': submitted && $v.email.$error }"></b-form-input>
-                                        <div v-if="submitted && $v.email.$error" class="invalid-feedback">
-                                            <span v-if="!$v.email.required">Email is required.</span>
-                                            <span v-if="!$v.email.email">Please enter valid email.</span>
-                                        </div>
-                                    </b-form-group>
+									<b-form @submit.prevent="tryToLogIn">
+										<b-form-group
+											id="input-group-1"
+											label="Email"
+											label-for="input-1"
+										>
+											<b-form-input
+												id="input-1"
+												v-model="email"
+												type="text"
+												placeholder="Enter email"
+												:class="{ 'is-invalid': submitted && $v.email.$error }"
+											></b-form-input>
+											<div
+												v-if="submitted && $v.email.$error"
+												class="invalid-feedback"
+											>
+												<span v-if="!$v.email.required"
+													>Email is required.</span
+												>
+												<span v-if="!$v.email.email"
+													>Please enter valid email.</span
+												>
+											</div>
+										</b-form-group>
 
-                                    <b-form-group id="input-group-2">
-                                        <div class="float-right">
-                                            <nuxt-link to="/account/forgot-password" class="text-muted">Forgot password?</nuxt-link>
-                                        </div>
-                                        <label for="input-2">Password</label>
-                                        <b-form-input id="input-2" v-model="password" type="password" placeholder="Enter password" :class="{ 'is-invalid': submitted && $v.password.$error }"></b-form-input>
-                                        <div v-if="submitted && !$v.password.required" class="invalid-feedback">Password is required.</div>
-                                    </b-form-group>
-                                    <div class="custom-control custom-checkbox">
-                                        <input id="customControlInline" type="checkbox" class="custom-control-input" />
-                                        <label class="custom-control-label" for="customControlInline">Remember me</label>
-                                    </div>
-                                    <div class="mt-3 text-right">
-                                        <b-button type="submit" variant="primary" class="w-sm">Log In</b-button>
-                                    </div>
-                                    <div class="mt-4 text-center">
-                                        <div class="signin-other-title">
-                                            <h5 class="font-size-14 mb-3 title">Sign in with</h5>
-                                        </div>
+										<b-form-group id="input-group-2">
+											<div class="float-right">
+												<nuxt-link
+													to="/account/forgot-password"
+													class="text-muted"
+													>Forgot password?</nuxt-link
+												>
+											</div>
+											<label for="input-2">Password</label>
+											<b-form-input
+												id="input-2"
+												v-model="password"
+												type="password"
+												placeholder="Enter password"
+												:class="{
+													'is-invalid': submitted && $v.password.$error,
+												}"
+											></b-form-input>
+											<div
+												v-if="submitted && !$v.password.required"
+												class="invalid-feedback"
+											>
+												Password is required.
+											</div>
+										</b-form-group>
+										<div class="custom-control custom-checkbox">
+											<input
+												id="customControlInline"
+												type="checkbox"
+												class="custom-control-input"
+											/>
+											<label
+												class="custom-control-label"
+												for="customControlInline"
+												>Remember me</label
+											>
+										</div>
+										<div class="mt-3 text-right">
+											<b-button type="submit" variant="primary" class="w-sm"
+												>Log In</b-button
+											>
+										</div>
+										<div class="mt-4 text-center">
+											<div class="signin-other-title">
+												<h5 class="font-size-14 mb-3 title">Sign in with</h5>
+											</div>
 
-                                        <ul class="list-inline">
-                                            <li class="list-inline-item">
-                                                <a href="javascript:void()" class="social-list-item bg-primary text-white border-primary">
-                                                    <i class="mdi mdi-facebook"></i>
-                                                </a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="javascript:void()" class="social-list-item bg-info text-white border-info">
-                                                    <i class="mdi mdi-twitter"></i>
-                                                </a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="javascript:void()" class="social-list-item bg-danger text-white border-danger">
-                                                    <i class="mdi mdi-google"></i>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
+											<ul class="list-inline">
+												<li class="list-inline-item">
+													<a
+														href="javascript:void()"
+														class="social-list-item bg-primary text-white border-primary"
+													>
+														<i class="mdi mdi-facebook"></i>
+													</a>
+												</li>
+												<li class="list-inline-item">
+													<a
+														href="javascript:void()"
+														class="social-list-item bg-info text-white border-info"
+													>
+														<i class="mdi mdi-twitter"></i>
+													</a>
+												</li>
+												<li class="list-inline-item">
+													<a
+														href="javascript:void()"
+														class="social-list-item bg-danger text-white border-danger"
+													>
+														<i class="mdi mdi-google"></i>
+													</a>
+												</li>
+											</ul>
+										</div>
 
-                                    <div class="mt-3 text-center">
-                                        <p class="mb-0">
-                                            Don't have an account ?
-                                            <nuxt-link to="/account/register" class="font-weight-medium text-primary">Signup now</nuxt-link>
-                                        </p>
-                                    </div>
-                                </b-form>
-                            </div>
-                            <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
-                    </div>
-                    <div class="mt-2 text-center">
-                        <p>
-                            Lamjaya Global Solusi © {{ new Date().getFullYear()}}
-                        </p>
-                    </div>
-                    <!-- end row -->
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-        </div>
-    </div>
-</div>
+										<div class="mt-3 text-center">
+											<p class="mb-0">
+												Don't have an account ?
+												<nuxt-link
+													to="/account/register"
+													class="font-weight-medium text-primary"
+													>Signup now</nuxt-link
+												>
+											</p>
+										</div>
+									</b-form>
+								</div>
+								<!-- end card-body -->
+							</div>
+							<!-- end card -->
+						</div>
+						<div class="mt-2 text-center">
+							<p>Lamjaya Global Solusi © {{ new Date().getFullYear() }}</p>
+						</div>
+						<!-- end row -->
+					</div>
+					<!-- end col -->
+				</div>
+				<!-- end row -->
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" module></style>
