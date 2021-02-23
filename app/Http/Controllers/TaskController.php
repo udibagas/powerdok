ecf2f0ede8f8647610cca771380621b3b0f9d0e5
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-02-23 09:52:49 +0700

    add document on show

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -76,7 +76,7 @@ class TaskController extends Controller
             'user',
             'assignees',
             'attachments',
-            // 'document',
+            'document',
             'comments',
             'approvals',
             'trackings'
