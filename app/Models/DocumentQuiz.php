e1afead698d313ce69d71348e4a693873b10d093
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-02-23 10:03:18 +0700

    update

---
+++ app/Models/DocumentQuiz.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentQuiz extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'document_id',
+        'question',
+        'choices',
+        'correct_answer'
+    ];
+
+    protected $casts = ['choices' => 'json'];
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
+}
---
+++ database/migrations/2021_02_23_095048_create_document_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('document_id');
+            $table->text('question');
+            $table->json('choices');
+            $table->tinyInteger('correct_answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_quizzes');
+    }
+}
