6239fd08e334589a0c7b3ae2cd1b4621cdecfeb2
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-03-08 09:28:44 +0700

    update

--- frontend/components/Attachments.vue
+++ frontend/components/Attachments.vue
@@ -1,71 +1,91 @@
 <template>
-  <div>
-    <div class="text-muted my-3">
-      <i class="el-icon-paperclip"></i>
-      {{ $t("Attachments") }}
-    </div>
+	<div>
+		<div class="text-muted my-3">
+			<i class="el-icon-paperclip"></i>
+			{{ $t("Attachments") }}
+		</div>
 
-    <div class="row">
-      <div class="col-md-4" v-for="(attachment, i) in attachments" :key="attachment.id">
-        <div class="media border rounded p-2 mb-3">
-          <a href="#" @click.prevent="index = i; visible = true">
-            <el-image
-              fit="cover"
-              class="mr-2 border"
-              style="height: 50px;width:50px"
-              :src="attachment.url"
-            >
-            <div slot="error" class="image-slot text-center" style="line-height:50px">
-              <i class="el-icon-document" style="font-size:30px;margin-top:10px"></i>
-            </div>
-            </el-image>
-          </a>
-          <div class="media-body text-truncate">
-            <div class="font-weight-bold"> {{attachment.name}} </div>
-            <div class="text-muted">
-              {{attachment.type}}
-            </div>
-            <div class="text-muted">
-              {{bytesToSize(attachment.size)}}
-              <a href="#" class="ml-3" @click.prevent="download(attachment.id)">
-                <i class="el-icon-download"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+		<div class="row">
+			<div
+				class="col-md-4"
+				v-for="(attachment, i) in attachments"
+				:key="attachment.id"
+			>
+				<div class="media border rounded p-2 mb-3">
+					<a
+						href="#"
+						@click.prevent="
+							index = i;
+							visible = true;
+						"
+					>
+						<el-image
+							fit="cover"
+							class="mr-2 border rounded"
+							style="height: 50px; width: 50px"
+							:src="attachment.url"
+						>
+							<div
+								slot="error"
+								class="image-slot text-center"
+								style="line-height: 50px"
+							>
+								<i
+									class="el-icon-document"
+									style="font-size: 30px; margin-top: 10px"
+								></i>
+							</div>
+						</el-image>
+					</a>
+					<div class="media-body text-truncate">
+						<div class="font-weight-bold">{{ attachment.name }}</div>
+						<div class="text-muted">
+							{{ attachment.type }}
+						</div>
+						<div class="text-muted">
+							{{ bytesToSize(attachment.size) }}
+							<a href="#" class="ml-3" @click.prevent="download(attachment.id)">
+								<i class="el-icon-download"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-    <vue-easy-lightbox
-      :visible="visible"
-      :imgs="attachments.map(a => a.url)"
-      :index="index"
-      @hide="visible = false"
-    ></vue-easy-lightbox>
-  </div>
+		<vue-easy-lightbox
+			:visible="visible"
+			:imgs="attachments.filter((a) => a.is_image).map((a) => a.url)"
+			:index="index"
+			@hide="visible = false"
+		></vue-easy-lightbox>
+	</div>
 </template>
 
 <script>
 export default {
-  props: ['attachments'],
-  data() {
-    return {
-      index: 0,
-      visible: false
-    }
-  },
+	props: ["attachments"],
+	data() {
+		return {
+			index: 0,
+			visible: false
+		};
+	},
 
-  methods: {
-    bytesToSize(bytes) {
-      var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-      if (bytes == 0) return '0 Byte';
-      var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-      return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-    },
+	methods: {
+		bytesToSize(bytes) {
+			var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+			if (bytes == 0) return "0 Byte";
+			var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+			return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+		},
 
-    download(id) {
-      window.open(`${this.$axios.defaults.baseURL}/api/download/${id}`, '_blank');
-    }
-  }
-}
+		download(id) {
+			window.open(
+				`${this.$axios.defaults.baseURL}/api/download/${id}`,
+				"_blank"
+			);
+		}
+	}
+};
 </script>
--- frontend/components/task/TaskExam.vue
+++ frontend/components/task/TaskExam.vue
@@ -1,151 +1,192 @@
 <template>
 	<div>
-    <el-card v-if="task.status != TASK_STATUS.FINISHED" class="mb-3 text-center" shadow="hover">
-      <a href="#" @click.prevent="confirmStart">
-        <h3> START EXAM</h3>
-      </a>
-    </el-card>
+		<el-card
+			v-if="task.status != TASK_STATUS.FINISHED"
+			class="mb-3 text-center"
+			shadow="hover"
+		>
+			<a href="#" @click.prevent="confirmStart">
+				<h3>START EXAM</h3>
+			</a>
+		</el-card>
 
-    <ExamDialog :task="task" :show="showDialog" />
+		<ExamDialog :task="task" :show="showDialog" />
 
-		<el-card class="mb-3" v-if="task.status == TASK_STATUS.FINISHED">
+		<el-card
+			class="mb-3"
+			v-if="
+				task.status == TASK_STATUS.FINISHED || task.status == TASK_STATUS.CLOSED
+			"
+		>
 			<div class="row">
-        <div class="col">
-          <div class="text-muted">
-            {{ $t("Score") }}
-          </div>
-          <h1>
-            {{ task.exam.score }}
-            <small>({{ task.exam.correct_answer }} / {{ task.exam.quizzes.length }})</small>
-          </h1>
-        </div>
-        <div class="col">
-          <div class="col text-muted">
-            {{ $t("Time") }}
-          </div>
-        </div>
-        <div class="col-md-2">
-          <div class="text-muted">
-            {{ $t("Status") }}
-          </div>
-          <h1 :class="{'text-success': task.exam.passed, 'text-danger': !task.exam.passed}">
-            {{ task.exam.passed ? $t("PASSED") : $t("FAILED") }}
-          </h1>
-        </div>
+				<div class="col">
+					<div class="text-muted">
+						{{ $t("Score") }}
+					</div>
+					<h1>
+						{{ task.exam.score }}
+						<small
+							>({{ task.exam.correct_answer }} /
+							{{ task.exam.quizzes.length }})</small
+						>
+					</h1>
+				</div>
+				<div class="col">
+					<div class="col text-muted">
+						{{ $t("Time") }}
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div class="text-muted">
+						{{ $t("Status") }}
+					</div>
+					<h1
+						:class="{
+							'text-success': task.exam.passed,
+							'text-danger': !task.exam.passed,
+						}"
+					>
+						{{ task.exam.passed ? $t("PASSED") : $t("FAILED") }}
+					</h1>
+				</div>
 			</div>
 		</el-card>
 
-    <div v-if="task.status == TASK_STATUS.FINISHED || task.status == TASK_STATUS.CLOSED">
-      <el-card
-        v-for="(e, index) in task.exam.quizzes"
-        class="mb-2"
-        :key="index"
-        :class="{
-          'border-success':
-            e.correct_answer == e.user_answer &&
-            task.status == TASK_STATUS.FINISHED,
-          'border-danger':
-            e.correct_answer != e.user_answer &&
-            task.status == TASK_STATUS.FINISHED
-        }"
-      >
-        <div class="d-flex">
-          <div class="mr-3">
-            <h4 class="text-muted">#{{ index + 1 }}</h4>
-            <i
-              style="font-size:30px"
-              :class="{
-                'el-icon-circle-check text-success' :
-                  e.correct_answer == e.user_answer && task.status == TASK_STATUS.FINISHED,
-                'el-icon-circle-close text-danger' :
-                  e.correct_answer != e.user_answer && task.status == TASK_STATUS.FINISHED
-              }"
-            ></i>
-          </div>
-          <div class="flex-grow-1">
-            <div class="d-flex">
-              <div class="mr-3 mb-3 flex-grow-1">
-                <h4>{{ e.question }}</h4>
+		<div
+			v-if="
+				task.status == TASK_STATUS.FINISHED || task.status == TASK_STATUS.CLOSED
+			"
+		>
+			<el-card
+				v-for="(e, index) in task.exam.quizzes"
+				class="mb-2"
+				:key="index"
+				:class="{
+					'border-success':
+						e.correct_answer == e.user_answer &&
+						task.status == TASK_STATUS.FINISHED,
+					'border-danger':
+						e.correct_answer != e.user_answer &&
+						task.status == TASK_STATUS.FINISHED,
+				}"
+			>
+				<div class="d-flex">
+					<div class="mr-3">
+						<h4 class="text-muted">#{{ index + 1 }}</h4>
+						<i
+							style="font-size: 30px"
+							:class="{
+								'el-icon-circle-check text-success':
+									e.correct_answer == e.user_answer &&
+									task.status == TASK_STATUS.FINISHED,
+								'el-icon-circle-close text-danger':
+									e.correct_answer != e.user_answer &&
+									task.status == TASK_STATUS.FINISHED,
+							}"
+						></i>
+					</div>
+					<div class="flex-grow-1">
+						<div class="d-flex">
+							<div class="mr-3 mb-3 flex-grow-1">
+								<h4>{{ e.question }}</h4>
 
-                <div class="row">
-                  <div class="col-md-2" v-for="(url, i) in e.attachments" :key="i">
-                    <a href="#" @click.prevent="index = i; visible = true">
-                      <el-image
-                        class="my-3 border mr-3 rounded"
-                        style="height: 100px;width:100px"
-                        :src="url"
-                      ></el-image>
-                    </a>
-                  </div>
-                </div>
-                <vue-easy-lightbox
-                  :visible="visible"
-                  :imgs="e.attachments"
-                  :index="index"
-                  @hide="visible = false"
-                ></vue-easy-lightbox>
-              </div>
-            </div>
+								<div class="row">
+									<div
+										class="col-md-2"
+										v-for="(url, i) in e.attachments"
+										:key="i"
+									>
+										<a
+											href="#"
+											@click.prevent="
+												index = i;
+												visible = true;
+											"
+										>
+											<el-image
+												class="my-3 border mr-3 rounded"
+												style="height: 100px; width: 100px"
+												:src="url"
+											></el-image>
+										</a>
+									</div>
+								</div>
+								<vue-easy-lightbox
+									:visible="visible"
+									:imgs="e.attachments"
+									:index="index"
+									@hide="visible = false"
+								></vue-easy-lightbox>
+							</div>
+						</div>
 
-            <div class="row">
-              <div v-for="(c, i) in e.choices" :key="i"
-                class="col-5 mb-3 d-flex"
-                :class="{
-                  'text-success': e.user_answer == e.correct_answer == i,
-                  'text-danger': e.user_answer != e.correct_answer && e.user_answer == i
-                }"
-              >
-                <strong class="mr-2">
-                  {{ ["A", "B", "C", "D"][i] }}.
-                </strong>
-                {{ e.choices[i] }}
-                <!-- <i v-if="e.user_answer == e.correct_answer == i" style="font-size:15px" class="el-icon-check text-success"></i>
+						<div class="row">
+							<div
+								v-for="(c, i) in e.choices"
+								:key="i"
+								class="col-5 mb-3 d-flex"
+								:class="{
+									'text-success font-weight-bold':
+										(e.user_answer == e.correct_answer) == i,
+									'text-danger font-weight-bold':
+										e.user_answer != e.correct_answer && e.user_answer == i,
+								}"
+							>
+								<strong class="mr-2"> {{ ["A", "B", "C", "D"][i] }}. </strong>
+								{{ e.choices[i] }}
+								<!-- <i v-if="e.user_answer == e.correct_answer == i" style="font-size:15px" class="el-icon-check text-success"></i>
                 <i v-if="e.user_answer != e.correct_answer && e.user_answer == i" style="font-size:15px" class="el-icon-close text-danger"></i> -->
-              </div>
-            </div>
-          </div>
-        </div>
-      </el-card>
-    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</el-card>
+		</div>
 	</div>
 </template>
 
 <script>
-import { TASK_STATUS, TASK_TYPE } from '@/store/modules/task'
+import { TASK_STATUS, TASK_TYPE } from "@/store/modules/task";
 
 export default {
-  props: ['task'],
+	props: ["task"],
 
-  data() {
-    return {
-      TASK_TYPE,
-      TASK_STATUS,
-      visible: false,
-      index: 0,
-      start: false,
-      time_start: null,
-      time_finished: null,
-      showDialog: false
-    }
-  },
+	data() {
+		return {
+			TASK_TYPE,
+			TASK_STATUS,
+			visible: false,
+			index: 0,
+			start: false,
+			time_start: null,
+			time_finished: null,
+			showDialog: false
+		};
+	},
 
-  computed: {
-    allowSubmitTask() {
-      return this.task.assignee_id == this.$auth.user.id &&
-        [TASK_STATUS.SUBMITTED, TASK_STATUS.ON_PROGRESS].includes(this.task.status);
-    },
-  },
+	computed: {
+		allowSubmitTask() {
+			return (
+				this.task.assignee_id == this.$auth.user.id &&
+				[TASK_STATUS.SUBMITTED, TASK_STATUS.ON_PROGRESS].includes(
+					this.task.status
+				)
+			);
+		},
 
-  methods: {
-    confirmStart() {
-      // TODO
-      this.start = true;
-      this.time_start = new Date();
-      this.showDialog = true
-    },
-  }
-}
+		taskIsDone() {
+			return [TASK_STATUS.FINISHED, TASK_STATUS.CLOSED].includes(
+				this.task.status
+			);
+		}
+	},
+
+	methods: {
+		confirmStart() {
+			this.start = true;
+			this.time_start = new Date();
+			this.showDialog = true;
+		}
+	}
+};
 </script>
-
-<style>
-</style>
